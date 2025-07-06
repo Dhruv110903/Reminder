f7f12f31b3fa1cfcf77411f44b159a0a0dfed8a8
Dhruv Vatsa <93207042+Dhruv110903@users.noreply.github.com>
2025-07-06 15:53:01 +0530

    Update index.py
    Signed-off-by: Dhruv Vatsa <93207042+Dhruv110903@users.noreply.github.com>

--- index.py
+++ index.py
@@ -476,7 +476,8 @@ def authenticate_gmail():
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
     else:
         flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-        creds = flow.run_local_server(port=8080)
+        # creds = flow.run_local_server(port=8080)
+        creds = flow.run_console()
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
     return build('gmail', 'v1', credentials=creds)
