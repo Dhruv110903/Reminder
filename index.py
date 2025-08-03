4e9ab4ef0cd080350d16b949bfa7806d27fd9574
Dhruv Vatsa <93207042+Dhruv110903@users.noreply.github.com>
2025-08-03 13:23:57 +0530

    Update index.py
    Signed-off-by: Dhruv Vatsa <93207042+Dhruv110903@users.noreply.github.com>

--- index.py
+++ index.py
@@ -20,15 +20,15 @@ from bs4 import BeautifulSoup
 import base64
 import re
 
-# # --- HIDE STREAMLIT STYLE ---
-# hide_st_style = """
-#             <style>
-#             #MainMenu {visibility: hidden;}
-#             footer {visibility: hidden;}
-#             header {visibility: hidden;}
-#             </style>
-#             """
-# st.markdown(hide_st_style, unsafe_allow_html=True)
+# --- HIDE STREAMLIT STYLE ---
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # It must be the first Streamlit command to run.
 st.set_page_config(
